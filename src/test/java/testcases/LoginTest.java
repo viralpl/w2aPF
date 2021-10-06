9841ff09cc9ea0b79622284a6f7b3758fd53851c
viral <viralpl002@gmail.com>
2021-10-06 10:43:59 +0530

    change to 2k

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -21,8 +21,8 @@ public class LoginTest {
 		
 		HomePage home = new HomePage(driver);
 		home.selGotoCourse().gotoBuynow();
-		Thread.sleep(1000);
-		//changes to 1000
+		Thread.sleep(2000);
+		//changes to 2000
 		home.apiGotoCourse().gotoBuynow();
 		
 	}
